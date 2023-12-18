52fbb5f940ed756d2ae090b543be1468fceaccf2
RP SOHAG <rpsohag.bd@gmail.com>
2023-12-18 21:33:15 +0600

    routing setup

---
+++ handlers/routeHandlers/notFoundHandler.js
@@ -0,0 +1,11 @@
+// module scaffolding
+
+const handler = {};
+
+handler.notFoundHandler = (requestProperties, callback) => {
+  callback(404, {
+    message: "Your requested url was not found",
+  });
+};
+
+module.exports = handler;
